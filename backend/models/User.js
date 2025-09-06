34699ab7a55326c34edeaa369ee74757aff90b98
Prabhav-Batra <prabhavbatra4@gmail.com>
2025-09-06 15:35:48 +0530

    Auth ver 1

---
+++ backend/models/User.js
@@ -0,0 +1,70 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+// Define the Person schema
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    age:{
+        type: Number
+    },
+    
+    mobile:{
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    address:{
+        type: String
+    },
+    
+    username: {//rollno
+        required: true,
+        type: String
+    },
+    password: {
+        required: true,
+        type: String
+    }
+});
+
+userSchema.pre('save', async function(next){
+    const user = this;
+
+    // Hash the password only if it has been modified (or is new)
+    if(!user.isModified('password')) return next();
+
+    try{
+        // hash password generation
+        const salt = await bcrypt.genSalt(10);
+
+        // hash password
+        const hashedPassword = await bcrypt.hash(user.password, salt);
+        
+        // Override the plain password with the hashed one
+        user.password = hashedPassword;
+        next();
+    }catch(err){
+        return next(err);
+    }
+})
+
+userSchema.methods.comparePassword = async function(candidatePassword){
+    try{
+        // Use bcrypt to compare the provided password with the hashed password
+        const isMatch = await bcrypt.compare(candidatePassword, this.password);
+        return isMatch;
+    }catch(err){
+        throw err;
+    }
+}
+
+// Create Person model
+const User = mongoose.model('User', userSchema);
+module.exports = User;
