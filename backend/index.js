d8af6289c1a7187a305ee66de5d33c9b8f28d449
Anurag Singh <anuragsingh2313@gmail.com>
2025-09-12 13:10:38 +0530

    removed cluster

--- backend/index.js
+++ backend/index.js
@@ -5,13 +5,13 @@ import { rateLimiterMiddleware } from "./middleware/rate-limiter.js";
 
 const app = express();
 
-app.use(express.json());
 app.use(
   cors({
     origin: "http://localhost:5173",
   }),
 );
+app.use(express.json());
+
 app.use("/health-check", rateLimiterMiddleware, healthCheckRoute);
 
-export default app;
-
+app.listen(3000);
