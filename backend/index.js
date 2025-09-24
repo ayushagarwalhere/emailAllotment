e6b4edaf6431804067115d0697c89a7903309920
sitanshu01 <nayansitanshu@gmail.com>
2025-09-25 00:47:42 +0530

    Integrated swagger doc for admin routes

---
+++ backend/index.js
@@ -0,0 +1,39 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser';
+import cors from "cors";
+import healthCheckRoute from "./src/routes/health-check.js";
+import { rateLimiterMiddleware } from "./middleware/rate-limiter.js";
+import routes from './src/routes/adminRoutes.js';
+import superAdminRoutes from './src/routes/superAdminRoutes.js';
+import studentRoutes from './routes/studentRoutes.js';
+import {swaggerUi,swaggerSpec} from './src/utils/swagger.js'
+
+const app = express();
+dotenv.config();
+
+let PORT = process.env.PORT || 5000 ;
+
+//swagger
+app.use("/apiDocs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+//middlewares
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({extended:true}));
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  }),
+);
+
+app.use("/health-check", rateLimiterMiddleware, healthCheckRoute);
+app.use('/admin', routes);
+app.use('/superadmin', superAdminRoutes);
+app.use("/students", studentRoutes);
+
+
+app.listen(PORT, ()=>{
+    console.log(`The server is running at http://localhost:${PORT}`);
+})
---
+++ backend/src/utils/swagger.js
@@ -0,0 +1,23 @@
+import YAML from 'yamljs';
+import path from 'path';
+import swaggerUi from 'swagger-ui-express';
+
+
+const superAdminDoc = YAML.load(path.join(__dirname,"./backend/src/docs/superAdmin.yaml"));
+const adminDoc = YAML.load(path.join(process.cwd(),"./src/docs/admin.yaml"));
+const studetDoc = YAML.load(path.join(__dirname,"./backend/src/docs/student.yaml"));
+
+const swaggerSpec = {
+    openapi: "3.0.0",
+    info:{
+        title : "Email Allotment",
+        version: "1.0.0"
+    },
+    paths:{
+        ...adminDoc.paths,
+        ...superAdminDoc.paths,
+        ...studetDoc.paths
+    }
+}
+
+export {swaggerSpec, swaggerUi}
